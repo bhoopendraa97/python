ace922f8d8509a0ea88e620c2d1a4082d31e8261
bhoopendraa97 <bhoopendraa77@gmail.com>
2024-11-17 14:19:00 +0530

    add_pattern

---
+++ pattern.py
@@ -0,0 +1,85 @@
+
+# print of tringle--------------------------------------------->
+'''n=int(input("enter row no"))
+i=1
+while i<=n:
+      print('*'*i + (n-i)*' ')
+      i=i+1 '''
+
+
+'''n=int(input("enter row no"))
+i=0
+while i<n:
+      print((n-i)*'*'+' '*i)
+      i=i+1'''
+
+
+'''n=int(input("enter row no"))
+i=0
+while i<n:
+      print(' '*i+'*'*(n-i))
+      i=i+1'''
+
+'''n=int(input("enter row no"))
+i=1
+while i<=n:
+      print((n-i)*' '+i*'*')
+      i=i+1'''
+
+'''n=int(input("enter row no"))
+i=0
+while i<n:
+      print((n-i)*' '+i*' *')
+      i=i+1'''
+
+'''n=int(input("enter row no"))
+i=0
+while i<n:
+      print(' *'*i+ (n-1)*' ')
+      i=i+1'''
+
+'''n=int(input("enter row no"))
+i=1
+while i<=n:
+      print('*'*i +' '*(n-1))
+      i=i+1 
+i=i-2
+while i>0:
+      print('*'*i)
+      i=i-1'''
+
+'''n=int(input("enter row no"))
+i=1
+while i<=n:
+      print(' '*(n-i)+'*'*i)
+      i=i+1 
+i=i-2
+while i>0:
+      print((n-i)*' '+'*'*i)
+      i=i-1'''
+
+
+'''n=int(input("enter row no"))
+i=n
+while i>0:
+      print('*'*i)
+      i=i-1
+i=i+2
+while i<=n:
+      print('*'*i)
+      i=i+1 '''
+
+'''n=int(input("enter row no"))
+i=n
+while i>0:
+      print(i*'*')
+      i=i-1
+i=i+2
+while i<=n:
+      print(i*'*')
+      i=i+1'''
+
+
+
+
+
