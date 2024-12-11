cfeeabd22be4ff4f08da2e36bb4d9948e6db1a0b
bhoopendraa97 <bhoopendraa77@gmail.com>
2024-12-11 21:09:06 +0530

    transfer

---
+++ transfer_statement.py
@@ -0,0 +1,68 @@
+
+'''n=int(input("enter the number"))
+i=1
+while i<=n:
+    if  i==5:
+        pass
+    else:
+        print(i)
+    i=i+1'''
+
+
+'''n=int(input("enter the number"))
+i=1
+while i<=n:
+    if  i==5:
+        i=i+1
+        continue
+    else:
+        print(i)
+    i=i+1'''
+
+
+'''n=int(input("enter the number"))
+i=1
+while i<=n:
+    if  i==5:     #ager aap ke code , mein
+        break
+    else:
+        print(i)
+    i=i+1'''
+
+
+#calculeter------------------------------>
+'''while True:
+     print("1 Addition\n 2 sub\n 3 div\n 4 mul\n 5 off")
+     n=int (input("enter what you want:"))
+     x=[1,2,3,4]
+     if  n in x:
+         x=int(input("enter 1st value"))
+         y=int(input("enter 2st value"))
+         if n==1:
+                print("Addition=",x+y)
+         elif n==2:  
+                print("sub=",x-y)
+         elif n==3:
+                print("Div=",x/y)
+         elif n==4:
+                print("mul=",x*y)
+     elif n==5:
+           break
+     else:
+           print("enter right no.")'''
+
+
+
+
+
+
+
+
+                
+
+
+
+
+
+
+
